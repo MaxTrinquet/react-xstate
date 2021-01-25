c7d5cb6b36824071085ddf94edc9e24c251926d8
Maxime Trinquet <maxime.trinquet@gmail.com>
2021-01-25 17:53:43 +0100

    setupclean

---
+++ src/components/app/index.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from '../home';
+
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
